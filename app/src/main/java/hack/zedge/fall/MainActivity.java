a999c45bb01529b437624892de8487f035458204
Alfredo <alfredo@perezfer.com>
2015-12-15 14:44:47 +0100

    screen breaking

--- app/src/main/java/hack/zedge/fall/MainActivity.java
+++ app/src/main/java/hack/zedge/fall/MainActivity.java
@@ -1,17 +1,19 @@
 package hack.zedge.fall;
 
+import android.app.Activity;
 import android.graphics.Color;
 import android.hardware.*;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private RelativeLayout mBackground;
     private int[] colors = { Color.parseColor("#F44336"), Color.parseColor("#E91E63"), Color.parseColor("#9C27B0") };
@@ -23,9 +25,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
 
         mBackground = (RelativeLayout) findViewById(R.id.background);
+        mBackground.setBackgroundDrawable(getResources().getDrawable(R.mipmap.screen_normal));
 
         mp = MediaPlayer.create(this, R.raw.wilhelm);
         mp2 = MediaPlayer.create(this, R.raw.breaking_glass);
@@ -44,8 +49,10 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void hitDetected(boolean fall) {
                 //Toast.makeText(MainActivity.this, "Hit detected", Toast.LENGTH_SHORT).show();
-                mBackground.setBackgroundColor(colors[random.nextInt(colors.length)]);
+                mBackground = (RelativeLayout) findViewById(R.id.background);
+                mBackground.setBackgroundDrawable(getResources().getDrawable(R.mipmap.screen_broken_04));
                 mp2.start();
+
             }
         });
     }
